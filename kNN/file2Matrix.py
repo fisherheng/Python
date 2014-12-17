e9cac5ad38dc63681c4a8e94eb495554bc4a4a2d
fisherheng <fisherheng@users.noreply.github.com>
2014-12-17 17:59:10 +0800

    Create file2Matrix.py

---
+++ kNN/file2Matrix.py
@@ -0,0 +1,14 @@
+def file2Matrix(filename):
+    fr = open(filename)
+    arrayOfLines = fr.readlines()
+    numberOfLines = len(arrayOfLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
