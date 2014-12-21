6c7d6c388b1702bb939ecced6ccb3f897f88438f
Henry.Zhang <shoumuren11@163.com>
2014-12-21 10:46:20 +0800

    修改:         knn.py 	添加了新的function，用于实际的应用

--- DatingAnaylis/knn.py
+++ DatingAnaylis/knn.py
@@ -58,7 +58,23 @@ def datingClassTest():
     numTestVecs = int(m * hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],4)      #The fouth value is the k value
         print("The classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
         if (classifierResult != datingLabels[i]): errorCount += 1.0
     print("The total error rate is: %f" % (errorCount/float(numTestVecs)))
+
+'''
+
+'''
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input(\
+                  "percentage of time spent play video games?"))
+    ffMiles = float(input("frequent flier miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('data/datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 4)
+    print("You will probably like the person: ", resultList[classifierResult - 1])
+
